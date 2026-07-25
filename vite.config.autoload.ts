fc0ad3dfe526c89ccfa2836ff9a8d436e95e3bf8
Tex Murphy <eng@visionary.cloud>
2026-07-25 12:40:45 -0400

    chore: upgrade vite and vitest

--- vite.config.autoload.ts
+++ vite.config.autoload.ts
@@ -6,6 +6,7 @@ import { defineConfig } from "vite";
 // Auto-init script build (IIFE for direct script tag usage)
 export default defineConfig({
   build: {
+    target: "es2015",
     emptyOutDir: false,
     lib: {
       entry: resolve(__dirname, "src/lib/autoload.ts"),
